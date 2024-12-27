f5f58a40b47e77f18415e788982a7146a89a34ab
BirdsWind <cecilia.humlelu@gmail.com>
2024-12-27 15:03:04 +0100

    Initial Commit

---
+++ RayCal/RayCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayCalApp.swift
+//  RayCal
+//
+//  Created by humlan on 2024-12-27.
+//
+
+import SwiftUI
+
+@main
+struct RayCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
